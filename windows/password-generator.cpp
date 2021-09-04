445df578c784dc1e0f73aeb49bcfd5a15011824a
wcyat <wongchunyat@pm.me>
2021-09-05 01:06:54 +0800

    copy to clipboard (windows only)

--- windows/password-generator.cpp
+++ windows/password-generator.cpp
@@ -10,12 +10,12 @@ using namespace std;
 void toClipboard(const std::string& s) {
     OpenClipboard(0);
     EmptyClipboard();
-    HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, s.size());
+    HGLOBAL hg = GlobalAlloc(GMEM_MOVEABLE, s.size() + 1);
     if (!hg) {
         CloseClipboard();
         return;
     }
-    memcpy(GlobalLock(hg), s.c_str(), s.size());
+    memcpy(GlobalLock(hg), s.c_str(), s.size() + 1);
     GlobalUnlock(hg);
     SetClipboardData(CF_TEXT, hg);
     CloseClipboard();
@@ -299,7 +299,7 @@ void core() {
 
 int main()
 {
-    cout << "g for generating passwords, o for options.\n";
+    cout << "g for generating passwords, o for options.\nPassword is copied to clipboard every time you generate.\n";
     init();
     core();
 }
