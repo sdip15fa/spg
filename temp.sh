d4bcdb3ff40fc30eed829a235c2958f9d8ac3f63
wcyat <wongchunyat@pm.me>
2021-09-07 23:03:25 +0800

    update

--- temp.sh
+++
@@ -1,1 +0,0 @@
-rm linux/build/Makefile linux/build/pg.cpp linux/build/pg.o linux/build/pg_options.txt linux/code/ClipboardXX/.gitignore linux/code/ClipboardXX/CMakeLists.txt linux/code/ClipboardXX/LICENSE linux/code/ClipboardXX/README.md linux/code/ClipboardXX/examples/copy.cpp linux/code/ClipboardXX/examples/paste.cpp linux/code/ClipboardXX/include/ClipboardXX.hpp linux/code/pg.cpp
