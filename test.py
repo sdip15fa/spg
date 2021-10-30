0be20e26ffa0ee80ea319d0cafbffbd55cc429aa
wcyat <9657150-wcyat@users.noreply.gitlab.com>
2021-10-30 16:25:45 +0800

    update

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ testfile = open("linux/pgtest.txt")
 for line in testfile:
   teststr = line
 os.system("rm linux/pgtest.cpp linux/pgtest linux/pgtest.txt linux/pg_options.txt")
-special_characters = "!@#$%^&*()-+?_=,<>/"
+special_characters = "!#$%&*?"
 testu = any(ele.isupper() for ele in teststr)
 testl = any(ele.islower() for ele in teststr)
 testn = False
@@ -28,6 +28,7 @@ for i in teststr:
   if i.isdigit():
     testn = True
 if not(any(c in special_characters for c in teststr) and testu and testl and testn and len(teststr) == 10):
+  print("output = " + teststr)
   raise ValueError("Test failed")
 else:
   print("test succeeded")
