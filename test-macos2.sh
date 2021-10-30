ad148942b170cac60f2b580cec213712b405e609
sdip15fa <91375035+sdip15fa@users.noreply.github.com>
2021-10-30 19:24:01 +0800

    Update test-macos2.sh

--- test-macos2.sh
+++ test-macos2.sh
@@ -3,8 +3,8 @@ g++ pg.cpp -o spg-macos.run
 chmod +x spg-macos.run
 ./spg-macos.run > spgtest.txt
 cd ..
-git config --global user.email "wcyat-azurepipelines@wcyat.me"
-git config --global user.name "Azure Pipelines"
+git config --global user.email "wcyat-githubactions@wcyat.me"
+git config --global user.name "Github Actions"
 git add -A
 git commit -a -m update
 git push origin master
